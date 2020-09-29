0697b17b8d71b7d5d431fb903b8694b9bd1bc7b0
Chiwon <chiwon99881@gmail.com>
2020-09-29 20:27:18 +0900

    #7 Custom User Model 2

--- users/models.py
+++ users/models.py
@@ -7,4 +7,21 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    # blank means required or non-required, while null means no data is OK?
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
